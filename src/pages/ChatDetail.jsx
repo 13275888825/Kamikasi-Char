b9073d8922f2ea9da36adad6e3c1941a4e4e16ee
wqh <wqh20010307@163.com>
2023-10-25 14:30:15 +0800

    增加chatDetail页面

--- src/pages/ChatDetail.jsx
+++ src/pages/ChatDetail.jsx
@@ -2,12 +2,12 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-25 09:45:13
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-10-25 14:01:58
+ * @LastEditTime: 2023-10-25 14:27:33
  * @FilePath: \Kamikasi Char\src\pages\ChatDetail.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useState } from 'react';
-import { Layout, Space, Avatar, Input, Button, Modal } from 'antd';
+import { Layout, Space, Avatar, Input, Button, Modal, Dropdown } from 'antd';
 import {
   UserOutlined,
   CopyOutlined,
@@ -16,15 +16,58 @@ import {
   RedditSquareFilled,
   TaobaoSquareFilled,
   ZhihuSquareFilled,
+  DownOutlined,
 } from '@ant-design/icons';
 import {
   LeftOutlined,
   ShareAltOutlined,
   MoreOutlined,
+  SaveOutlined,
+  FolderViewOutlined,
+  FolderAddOutlined,
+  DeleteOutlined,
 } from '@ant-design/icons';
 import './ChatDetail.css';
 import { useNavigate } from 'react-router-dom';
 const { Search } = Input;
+const items = [
+  {
+    label: (
+      <span className='labelItem'>
+        <SaveOutlined className='labelIcon' />
+        Save and Start New Chat
+      </span>
+    ),
+    key: '1',
+  },
+  {
+    label: (
+      <span className='labelItem'>
+        <FolderViewOutlined className='labelIcon' />
+        View Saved Chats
+      </span>
+    ),
+    key: '2',
+  },
+  {
+    label: (
+      <span className='labelItem'>
+        <FolderAddOutlined className='labelIcon' />
+        Create Post
+      </span>
+    ),
+    key: '3',
+  },
+  {
+    label: (
+      <span className='labelItem'>
+        <DeleteOutlined className='labelIcon' />
+        Remove Messages
+      </span>
+    ),
+    key: '4',
+  },
+];
 const ChatDetail = () => {
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -40,6 +83,9 @@ const ChatDetail = () => {
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+  };
+  const more = () => {
+    console.log('more');
   };
   return (
     <div className='chat'>
@@ -60,7 +106,17 @@ const ChatDetail = () => {
           </div>
           <div className='right'>
             <ShareAltOutlined className='icon' onClick={share} />
-            <MoreOutlined className='icon' />
+            <Dropdown
+              menu={{
+                items,
+              }}
+            >
+              <a onClick={e => e.preventDefault()}>
+                <Space>
+                  <MoreOutlined className='icon' onClick={more} />
+                </Space>
+              </a>
+            </Dropdown>
           </div>
         </div>
         <div className='remember'>
@@ -89,7 +145,7 @@ const ChatDetail = () => {
         </div>
       </div>
       <div className='foot'>
-        <Input defaultValue='Combine input and button' />
+        <Input placeholder='type a message' />
         <Button type='primary'>
           <SendOutlined />
         </Button>
