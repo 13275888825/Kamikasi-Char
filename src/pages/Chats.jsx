33523fb8b669d54490524fb12c8f80edb3359ecc
wqh <wqh20010307@163.com>
2023-10-27 10:52:03 +0800

    chats页面跳转到聊天详情

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: wqh wqh20010307@163.com
+ * @Date: 2023-10-19 16:54:51
+ * @LastEditors: wqh wqh20010307@163.com
+ * @LastEditTime: 2023-10-27 10:47:44
+ * @FilePath: \Kamikasi Char\src\pages\Chats.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import { Avatar, List } from 'antd';
 import { useNavigate } from 'react-router-dom';
@@ -32,7 +40,7 @@ const App = () => {
   const navigate = useNavigate();
   const chatsItem = item => {
     console.log(item.title, 'item');
-    navigate(`/chats/char?${item.title}`);
+    navigate('/chatDetail');
   };
   return (
     <List
