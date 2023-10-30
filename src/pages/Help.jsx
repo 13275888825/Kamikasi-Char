8651e6cf2950cc533e1722ec53270ec06f08f4ef
wqh <wqh20010307@163.com>
2023-10-30 11:16:13 +0800

    help页面增加视频播放

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -2,16 +2,16 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-23 10:49:03
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-10-23 10:52:42
+ * @LastEditTime: 2023-10-30 11:14:05
  * @FilePath: \web\src\pages\Help.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from 'react';
-
+import VideoPlay from '../components/VideoPlayer';
 const Help = () => {
   return (
     <div className='home'>
-      <h1>Help页面</h1>
+      <VideoPlay />
     </div>
   );
 };
