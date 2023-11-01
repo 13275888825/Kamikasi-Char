af6303925586ea6f2f7557d1a3e8d1975f6e9f0d
wqh <wqh20010307@163.com>
2023-11-01 17:51:23 +0800

    登录成功token的配置请求拦截器的封装

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,7 +2,7 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-23 16:27:20
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-10-25 15:01:04
+ * @LastEditTime: 2023-11-01 17:47:49
  * @FilePath: \Kamikasi Char\src\components\Login\index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,30 +12,82 @@ import { Avatar, Space, Dropdown } from 'antd';
 import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const navigate = useNavigate();
+  const authToken = localStorage.getItem('token');
   const toLogin = () => {
     navigate('/login');
   };
   const toProfile = () => {
     navigate('/profile');
   };
-  const items = [
-    {
-      label: (
-        <span style={{ color: '#000' }} onClick={toLogin}>
-          登录
-        </span>
-      ),
-      key: '1',
-    },
-    {
-      label: (
-        <span style={{ color: '#000' }} onClick={toProfile}>
-          个人信息
-        </span>
-      ),
-      key: '2',
-    },
-  ];
+  const Logout = () => {
+    navigate('/login');
+  };
+  const items = [];
+  if (!authToken) {
+    items.push(
+      {
+        label: (
+          <span style={{ color: '#000' }} onClick={toProfile}>
+            个人信息
+          </span>
+        ),
+        key: '1',
+      },
+      {
+        label: (
+          <span style={{ color: '#000' }} onClick={toLogin}>
+            登录
+          </span>
+        ),
+        key: '2',
+      }
+    );
+  } else {
+    items.push(
+      {
+        label: (
+          <span style={{ color: '#000' }} onClick={toProfile}>
+            个人信息
+          </span>
+        ),
+        key: '1',
+      },
+      {
+        label: (
+          <span style={{ color: '#000' }} onClick={Logout}>
+            退出登录
+          </span>
+        ),
+        key: '2',
+      }
+    );
+  }
+  // const items = [
+  //   {
+  //     label: (
+  //       <span style={{ color: '#000' }} onClick={toLogin}>
+  //         登录
+  //       </span>
+  //     ),
+  //     key: '1',
+  //   },
+  //   {
+  //     label: (
+  //       <span style={{ color: '#000' }} onClick={toProfile}>
+  //         个人信息
+  //       </span>
+  //     ),
+  //     key: '2',
+  //   },
+  //   {
+  //     label: (
+  //       <span style={{ color: '#000' }} onClick={Logout}>
+  //         退出登录
+  //       </span>
+  //     ),
+  //     key: '3',
+  //   },
+  // ];
   return (
     <Dropdown
       menu={{
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-23 15:19:45
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-11-01 17:15:35
+ * @LastEditTime: 2023-11-01 17:30:45
  * @FilePath: \KamikasiChar\src\pages\Login.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -36,6 +36,7 @@ const Login = ({ setAuthToken }) => {
         console.log('Response:', response.data);
         if (response.data.token) {
           navigate('/');
+          localStorage.setItem('token', response.data.token);
         }
       })
       .catch(error => {
