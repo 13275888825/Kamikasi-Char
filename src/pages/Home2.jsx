754bdea89a2e8bf22ef96a04413970e1092499c3
wqh <wqh20010307@163.com>
2023-11-02 17:14:14 +0800

    调整样式

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -2,7 +2,7 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-19 16:54:51
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-10-31 11:15:05
+ * @LastEditTime: 2023-11-02 17:13:26
  * @FilePath: \web\src\pages\Chats.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -52,7 +52,7 @@ export default function App() {
     navigate('/chatDetail');
   };
   return (
-    <div style={{ width: '90%', position: 'relative', left: '5%' }}>
+    <div>
       <div style={{ marginLeft: '5%' }}>
         <h3 style={{ color: 'rgba(229, 224, 216, 0.85)' }}>
           Continue chatting
