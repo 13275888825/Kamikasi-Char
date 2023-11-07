e5660527e594dae2ee590f518357c534c7a41ca3
wqh <wqh20010307@163.com>
2023-11-07 11:03:37 +0800

    修改profile的页面跳转

--- src/pages/ProfileSettings.jsx
+++ src/pages/ProfileSettings.jsx
@@ -2,7 +2,7 @@
  * @Author: wqh wqh20010307@163.com
  * @Date: 2023-10-26 09:36:33
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-10-26 11:50:38
+ * @LastEditTime: 2023-11-07 11:03:00
  * @FilePath: \Kamikasi Char\src\pages\ProfileSettings.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -72,7 +72,7 @@ const ProfileSettings = () => {
   };
   const navigate = useNavigate();
   const toHome = () => {
-    navigate('/');
+    navigate('/profile');
   };
   return (
     <div
