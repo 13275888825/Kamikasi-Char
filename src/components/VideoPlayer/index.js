aa57626405a5b9245d54fd87f665394551b387c5
wqh <wqh20010307@163.com>
2023-11-09 17:57:42 +0800

    使用react-player

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React, { useRef, useEffect } from 'react';
+import Hls from 'hls.js';
 
 const VideoPlayer = ({ hlsUrl }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    if (videoElement) {
+      if (Hls.isSupported()) {
+        console.log('支持');
+        const hls = new Hls();
+        hls.loadSource(hlsUrl);
+        console.log(videoElement, 'lllll');
+        hls.attachMedia(videoElement);
+      } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+        videoElement.src = hlsUrl;
+      }
+    }
+  }, [hlsUrl]);
+
   return (
     <div>
-      <ReactPlayer url={hlsUrl} controls width='640px' height='360px' />
+      <video ref={videoRef} controls width='450' height='450' />
     </div>
   );
 };
--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import VideoPlayer from '../components/VideoPlayer';
+import ReactPlayer from 'react-player';
 
 const App = () => {
-  const hlsUrl = '../';
-
+  const hlsUrl =
+    'http://1252093142.vod2.myqcloud.com/4704461fvodcq1252093142/48c8a9475285890781000441992/playlist.m3u8';
+  const hlsUrl2 = '/project/RealChar/playlist.m3u8';
+  const hlsUrl3 = 'http://127.0.0.1:8080/playlist.m3u8';
   return (
     <div>
-      <h1>ReactPlayer HLS Video Player</h1>
-      <VideoPlayer hlsUrl={hlsUrl} />
+      <h1>React Hls.js Player</h1>
+      <VideoPlayer hlsUrl={hlsUrl3} />
+      {/* <ReactPlayer
+        url={hlsUrl}
+        controls
+        width='640px'
+        height='360px'
+      ></ReactPlayer> */}
     </div>
   );
 };
