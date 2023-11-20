48a783c9e01fcb39bcccd2677a99b2fa19032e69
wqh <wqh20010307@163.com>
2023-11-20 13:15:00 +0800

    整理后上传

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,109 +1,44 @@
 /*
  * @Author: wqh wqh20010307@163.com
- * @Date: 2023-11-09 19:00:46
+ * @Date: 2023-11-17 15:34:04
  * @LastEditors: wqh wqh20010307@163.com
- * @LastEditTime: 2023-11-17 17:13:01
- * @FilePath: \KamikasiChar\src\pages\Help.jsx
+ * @LastEditTime: 2023-11-20 13:10:59
+ * @FilePath: \Kamikasi-Char\src\pages\Help.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect, useRef } from 'react';
-// import Hls from 'hls.js';
-import Videojs from 'video.js';
-import 'videojs-contrib-hls.js';
-import 'video.js/dist/video-js.css';
 import useWebSocket from 'react-use-websocket';
-window.videojs = Videojs;
-import('video.js/dist/lang/zh-CN.js');
 
-const HlsVideoPlayer = () => {
-  const videoInstance = useRef(null);
-  const videoInstance2 = useRef(null);
-  const socketUrl = 'ws://localhost:8080';
-  const { sendMessage, lastMessage } = useWebSocket(socketUrl);
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+  const { sendMessage, lastMessage } = useWebSocket('ws://localhost:8080');
+
   useEffect(() => {
-    console.log('123');
-    const videoInstance = Videojs(
-      'videoBox',
-      {
-        muted: true,
-        preload: true,
-        language: 'zh-CN',
-        // fluid: true,
-        sources: [
-          //{
-          //  type: 'application/x-mpegURL',
-          //  src: 'http://192.168.1.62:1935/live/123.m3u8',
-          //},
-          {
-            type: 'video/mp4',
-            src: '/Obama.mp4',
-          },
-        ],
-        notSupportedMessage: '无法播放',
-      },
-      () => {
-        videoInstance.play();
+    sendMessage('前端发来的');
+    sendMessage('/videostart');
+    if (lastMessage) {
+      console.log('从服务器接收到消息:', lastMessage.data);
+      // 处理从服务器接收到的视频数据
+      const videoData = lastMessage.data;
+      // 处理视频数据，这取决于你的视频显示方式
+      // 在这个例子中，假设你有一个 video 元素来显示视频
+      const videoBlob = new Blob([videoData], { type: 'video/mp4' });
+      console.log(videoBlob, 'videoBlob');
+      const videoUrl = URL.createObjectURL(videoData);
+      if (videoRef.current) {
+        videoRef.current.src = videoUrl;
       }
-    );
-    const videoInstance2 = Videojs(
-      'videoBox2',
-      {
-        muted: true,
-        preload: true,
-        language: 'zh-CN',
-        // fluid: true,
-        sources: [
-          {
-            type: 'application/x-mpegURL',
-            src: '/output.m3u8',
-          },
-        ],
-        notSupportedMessage: '无法播放',
-      },
-      () => {
-        videoInstance2.play();
-      }
-    );
-  }, [videoInstance, videoInstance2]);
-  useWebSocket(socketUrl, {
-    onOpen: () => {
-      console.log('WebSocket connection established.');
-    },
-    onMessage: message => {
-      console.log(message.data, '00000');
-      // eslint-disable-next-line no-undef
-      console.log(`Received from server: ${message.data}`);
-    },
-    onClose: () => {
-      console.log('Disconnected from WebSocket');
-    },
-  });
+      console.log(videoRef.current.src, 'src');
+    }
+  }, [lastMessage]);
+
   return (
-    <>
+    <div>
       <div>
-        <video
-          autoPlay
-          loop
-          className='video-js vjs-default-skin'
-          width={400}
-          height={400}
-          id='videoBox'
-          controls
-        />
+        <video ref={videoRef} controls width='600' height='400' />
       </div>
-      <div>
-        <video
-          autoPlay
-          loop
-          className='video-js vjs-default-skin'
-          width={400}
-          height={400}
-          id='videoBox2'
-          controls
-        />
-      </div>
-    </>
+    </div>
   );
 };
 
-export default HlsVideoPlayer;
+export default VideoPlayer;
---
+++ src/server/app.js
@@ -0,0 +1,51 @@
+/*
+ * @Author: wqh wqh20010307@163.com
+ * @Date: 2023-11-17 15:32:57
+ * @LastEditors: wqh wqh20010307@163.com
+ * @LastEditTime: 2023-11-20 13:09:06
+ * @FilePath: \Kamikasi-Char\src\my-node-api\app.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const fs = require('fs');
+const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(cors());
+
+wss.on('connection', ws => {
+  console.log('WebSocket连接已建立');
+
+  // 读取MP4文件
+  const videoStream = fs.createReadStream('./public/Obama.mp4');
+
+  // 监听WebSocket连接关闭事件
+  ws.on('close', () => {
+    console.log('WebSocket连接已关闭');
+    videoStream.destroy();
+  });
+
+  // 监听WebSocket客户端发送的消息
+  ws.on('message', message => {
+    console.log('从客户端接收到消息:', message);
+
+    if (message == '/videostart') {
+      // 处理"/videostart"消息，开始向客户端发送视频数据
+      console.log('开始播放视频...');
+
+      videoStream.on('data', chunk => {
+        // 将视频数据推送到WebSocket连接
+        ws.send(chunk);
+      });
+    }
+  });
+});
+
+server.listen(8080, () => {
+  console.log('服务器正在监听端口8080');
+});
