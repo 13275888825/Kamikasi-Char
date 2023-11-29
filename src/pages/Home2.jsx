c2c3e52e21706d121898ab86b9161a69c4b08f94
wqh <wqh20010307@163.com>
2023-11-29 14:32:06 +0800

    人物卡片的分类

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -7,8 +7,9 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect } from 'react';
-import { Col, Row, Card } from 'antd';
+import { Col, Row, Card, Tabs } from 'antd';
 const { Meta } = Card;
+const { TabPane } = Tabs;
 import { getHostName } from '../utils/urlUtils';
 import './Chats.css';
 import queryString from 'query-string';
@@ -66,6 +67,182 @@ export default function App() {
   //     setCard(res.data);
   //   });
   // };
+  const renderTabs = () => {
+    // 获取关键人物列表
+    const socialFigures = list.filter(item => {
+      const keywords = [
+        'Mark Zuckerberg',
+        'Arnold Schwarzenegger',
+        'Ion Stoica',
+        'Keanu Reeves',
+        'Sam Altman',
+        'Elon musk',
+        'Steve Jobs',
+      ];
+      return keywords.includes(item.name);
+    });
+
+    // 根据关键人物生成 Tabs 和对应的 Card
+    return (
+      <Tabs defaultActiveKey='1' type='card' style={{ marginTop: '12px' }}>
+        <TabPane tab='社会名流' key='1'>
+          <Row gutter={[16, 16]}>
+            {socialFigures.map((item, key) => (
+              <Col
+                span={4}
+                xxl={4}
+                xl={4}
+                lg={6}
+                md={6}
+                sm={8}
+                xs={12}
+                key={item.character_id}
+              >
+                <Card
+                  onClick={() => toDetail(item)}
+                  hoverable
+                  style={{
+                    width: 200,
+                    height: 200,
+                    paddingTop: '10px',
+                  }}
+                  cover={
+                    <div>
+                      <img
+                        style={{
+                          width: '80%',
+                          height: '160px',
+                          marginLeft: '10%',
+                        }}
+                        src={item.image_url}
+                      />
+                      <div style={{ textAlign: 'center' }}>{item.name}</div>
+                    </div>
+                  }
+                ></Card>
+              </Col>
+            ))}
+          </Row>
+        </TabPane>
+      </Tabs>
+    );
+  };
+  const renderTabs2 = () => {
+    // 获取关键人物列表
+    const animeCharacters = list.filter(item => {
+      const keywords = [
+        'Raiden Shogun And Ei Avatar',
+        'Helen Inhabitants Zone',
+        'Raiden Shogun And Ei',
+        'Loki',
+      ];
+      return keywords.includes(item.name);
+    });
+
+    // 根据关键人物生成 Tabs 和对应的 Card
+    return (
+      <Tabs defaultActiveKey='1' type='card' style={{ marginTop: '12px' }}>
+        <TabPane tab='动漫角色' key='2'>
+          <Row gutter={[16, 16]}>
+            {animeCharacters.map((item, key) => (
+              <Col
+                span={4}
+                xxl={4}
+                xl={4}
+                lg={6}
+                md={6}
+                sm={8}
+                xs={12}
+                key={item.character_id}
+              >
+                <Card
+                  onClick={() => toDetail(item)}
+                  hoverable
+                  style={{
+                    width: 200,
+                    height: 200,
+                    paddingTop: '10px',
+                  }}
+                  cover={
+                    <div>
+                      <img
+                        style={{
+                          width: '80%',
+                          height: '160px',
+                          marginLeft: '10%',
+                        }}
+                        src={item.image_url}
+                      />
+                      <div style={{ textAlign: 'center' }}>{item.name}</div>
+                    </div>
+                  }
+                ></Card>
+              </Col>
+            ))}
+          </Row>
+        </TabPane>
+      </Tabs>
+    );
+  };
+  const renderTabs3 = () => {
+    // 获取关键人物列表
+    const virtualCharacters = list.filter(item => {
+      const keywords = [
+        'Unreal Speech',
+        'The Dolphin',
+        'The Cat',
+        'Bruce Wayne',
+        'fangtaikefuxiaofang',
+      ];
+      return keywords.includes(item.name);
+    });
+
+    // 根据关键人物生成 Tabs 和对应的 Card
+    return (
+      <Tabs defaultActiveKey='1' type='card' style={{ marginTop: '12px' }}>
+        <TabPane tab='虚拟角色' key='3'>
+          <Row gutter={[16, 16]}>
+            {virtualCharacters.map((item, key) => (
+              <Col
+                span={4}
+                xxl={4}
+                xl={4}
+                lg={6}
+                md={6}
+                sm={8}
+                xs={12}
+                key={item.character_id}
+              >
+                <Card
+                  onClick={() => toDetail(item)}
+                  hoverable
+                  style={{
+                    width: 200,
+                    height: 200,
+                    paddingTop: '10px',
+                  }}
+                  cover={
+                    <div>
+                      <img
+                        style={{
+                          width: '80%',
+                          height: '160px',
+                          marginLeft: '10%',
+                        }}
+                        src={item.image_url}
+                      />
+                      <div style={{ textAlign: 'center' }}>{item.name}</div>
+                    </div>
+                  }
+                ></Card>
+              </Col>
+            ))}
+          </Row>
+        </TabPane>
+      </Tabs>
+    );
+  };
+
   const toDetail = async item => {
     console.log(item, 'selectedCharacter');
     const compressedCharacter = lz.compressToEncodedURIComponent(
@@ -112,48 +289,10 @@ export default function App() {
   };
   return (
     <div>
-      <div style={{ marginLeft: '5%' }}>
-        <h3 style={{ color: 'rgba(229, 224, 216, 0.85)' }}>
-          Continue chatting
-        </h3>
-        <Row gutter={[16, 16]}>
-          {list.map((item, key) => (
-            // eslint-disable-next-line react/jsx-key
-            <Col
-              span={4}
-              xxl={4}
-              xl={4}
-              lg={6}
-              md={6}
-              sm={8}
-              xs={12}
-              key={item.character_id}
-            >
-              <Card
-                onClick={() => toDetail(item)}
-                hoverable
-                style={{
-                  width: 200,
-                  height: 200,
-                  paddingTop: '10px',
-                }}
-                cover={
-                  <div>
-                    <img
-                      style={{
-                        width: '80%',
-                        height: '160px',
-                        marginLeft: '10%',
-                      }}
-                      src={item.image_url}
-                    />
-                    <div style={{ textAlign: 'center' }}>{item.name}</div>
-                  </div>
-                }
-              ></Card>
-            </Col>
-          ))}
-        </Row>
+      <div style={{ marginTop: '15px' }}>
+        {renderTabs()}
+        {renderTabs2()}
+        {renderTabs3()}
         {/* <Divider style={{ background: '#fff' }} />
         <Button.Group color='secondary'>
           <Button className='btns'>One</Button>
