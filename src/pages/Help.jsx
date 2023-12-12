a1a67be2f59ee9c9bd50e2fb6acc42c4fb594c19
wqh <wqh20010307@163.com>
2023-12-12 11:46:06 +0800

    根据后台的数据渲染动作

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -8,17 +8,46 @@ import { FBXLoader } from 'three/addons/loaders/FBXLoader';
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
 import Stats from 'three/addons/libs/stats.module.js';
 const Help = () => {
-  const [tracks, setTracks] = useState('')
+  const [tracks, setTracks] = useState('');
   const containerRef = useRef();
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
+  );
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   const stats = new Stats();
   const clock = new THREE.Clock();
   const [messages, setMessages] = useState([]);
   const [socket, setSocket] = useState(null);
+  const [wsUrl, setWsUrl] = useState('');
+  const [status, setStatus] = useState(0);
+  var actionStatus;
   let mixer;
 
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8081');
+    // 监听连接打开事件
+    newSocket.addEventListener('open', event => {
+      console.log('WebSocket连接已打开', event);
+    });
+
+    // 监听接收消息事件
+    newSocket.addEventListener('message', event => {
+      console.log(event.data,'data');
+      localStorage.setItem('status',event.data)
+    });
+    console.log('before');
+    init();
+    console.log('after');
+    animate();
+
+    return () => {
+      // Clean up resources (if needed) when the component is unmounted
+    };
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
   const init = () => {
     camera.position.set(100, 200, 300);
     scene.background = new THREE.Color(0xa0a0a0);
@@ -48,40 +77,46 @@ const Help = () => {
     grid.material.opacity = 0.2;
     grid.material.transparent = true;
     scene.add(grid);
+    const status = localStorage.getItem('status');
+    console.log(status,'status');
 
+    const url = getStatus(status);
+    console.log(url,'url');
+    console.log(getStatus('-1'));
     const loader = new FBXLoader();
-    //动作
-    loader.load('/api4/fbx/Monica_Talk01.fbx',
-      (object) => {
-        console.log(object, 'obj')
-        setTracks(object.animations[0].tracks)
-        //模型
-        loader.load('/api4/fbx/Monica_Expression.fbx', (fbx) => {
-          mixer = new THREE.AnimationMixer(fbx);
-          fbx.animations[0] = object.animations[0]
-          // console.log(fbx.animations[0].tracks,'mmmmmm');
-          const action = mixer.clipAction(fbx.animations[0]);
-          action.play();
+    loader.load('/api4/fbx/Monica_handshakes.fbx', object => {
+      console.log(object, 'obj');
+      setTracks(object.animations[0].tracks);
+      //模型
+      loader.load('/api4/fbx/Monica_Expression.fbx', fbx => {
+        mixer = new THREE.AnimationMixer(fbx);
+        // console.log(object.animations[0].tracks,'nnnnnn');
+        // console.log(fbx.animations[0].tracks,'lllll');
+        fbx.animations[0] = object.animations[0];
+        // console.log(fbx.animations[0].tracks,'mmmmmm');
+        const action = mixer.clipAction(fbx.animations[0]);
+        console.log(action, 'aaaaa');
+        action.play();
 
-          fbx.traverse((child) => {
-            if (child.isMesh) {
-              child.castShadow = true;
-              child.receiveShadow = true;
-            }
-          });
-
-          scene.add(fbx);
+        fbx.traverse(child => {
+          if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
         });
-        // action.play()
-        // object.traverse((child) => {
-        //   if (child.isMesh) {
-        //     child.castShadow = true;
-        //     child.receiveShadow = true;
-        //   }
-        // });
-        // // return 1;
-        // scene.add(object);
+
+        scene.add(fbx);
       });
+      // action.play()
+      // object.traverse((child) => {
+      //   if (child.isMesh) {
+      //     child.castShadow = true;
+      //     child.receiveShadow = true;
+      //   }
+      // });
+      // // return 1;
+      // scene.add(object);
+    });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
@@ -114,27 +149,19 @@ const Help = () => {
     stats.update();
   };
 
-  useEffect(() => {
-    const newSocket = new WebSocket('ws://localhost:8081');
-     // 监听连接打开事件
-     newSocket.addEventListener('open', (event) => {
-      console.log('WebSocket连接已打开', event);
-    });
-
-    // 监听接收消息事件
-    newSocket.addEventListener('message', (event) => {
-      const message = JSON.parse(event.data);
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
-    console.log('before');
-    init();
-    console.log('after');
-    animate();
-
-    return () => {
-      // Clean up resources (if needed) when the component is unmounted
-    };
-  }, []); // Empty dependency array ensures useEffect runs only once on mount
+  const getStatus = (val)=>{
+   console.log(val,'pppp');
+   return (
+    val == '-1' ?  "/api4/fbx/Monica_Standing%20Greeting.fbx":
+    val == '1'  ?  "/api4/fbx/Monica_Talk01.fbx":
+    val == '2'  ?  "/api4/fbx/Monica01_allaboutthatbass.fbx": 
+    val == '3'  ? "/api4//fbx/Monica_Chat_Sitting.fbx":
+    val == '4'  ? "/api4/fbx/Monica_handshakes.fbx":
+    val == '5'  ? "/api4/fbx/Monica_LeaningChat.fbx":
+    val == '6'  ? "/api4/fbx/Monica_MoveGreet.fbx":
+    val == '7'  ? "/api4/fbx/Monica_StandingChat.fbx":"/api4/fbx/Monica_Standing%20Greeting.fbx"
+   )
+  }
 
   return <div ref={containerRef} />;
 };
